d0cf49ee34ac913882f723202851ee9852a8f695
Rituparna Biswas <145699840+rituparna25@users.noreply.github.com>
2025-03-16 17:13:55 +0530

    first commit

---
+++ src/components/sections/Contact.jsx
@@ -0,0 +1,175 @@
+import React, { useRef } from "react";
+import styled from "styled-components";
+import emailjs from "@emailjs/browser";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  z-index: 1;
+  align-items: center;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1100px;
+  gap: 12px;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 52px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+    margin-top: 12px;
+    font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_secondary};
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const ContactForm = styled.form`
+  width: 95%;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(17, 25, 40, 0.83);
+  border: 1px solid rgba(255, 255, 255, 0.125);
+  padding: 32px;
+  border-radius: 12px;
+  box-shadow: rgba(23, 92, 230, 0.1) 0px 4px 24px;
+  margin-top: 28px;
+  gap: 12px;
+`;
+
+const ContactTitle = styled.div`
+  font-size: 28px;
+  margin-bottom: 6px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const ContactInput = styled.input`
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary + 50};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+`;
+
+const ContactInputMessage = styled.textarea`
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary + 50};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+`;
+
+const ContactButton = styled.input`
+  width: 100%;
+  text-align: center;
+  background: hsla(271, 100%, 50%, 1);
+  padding: 13px 16px;
+  margin-top: 2px;
+  border-radius: 12px;
+  border: none;
+  color: ${({ theme }) => theme.text_primary};
+  font-size: 18px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+const Contact = () => {
+  const form = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_jbqtxvj",
+        "template_yis7nal",
+        form.current,
+        "JIiM2wDoQecZqxRGq"
+      )
+      .then(
+        (result) => {
+          alert("Message Sent Successfully!");
+          form.current.reset(); // Reset form after successful submission
+        },
+        (error) => {
+          alert("Failed to send message. Please try again later.");
+          console.error(error);
+        }
+      );
+  };
+
+  return (
+    <Container id="Contact">
+      <Wrapper>
+        <Title>Contact</Title>
+        <Desc style={{ marginBottom: "40px" }}>
+          Feel free to reach out to me for any questions or opportunities!
+        </Desc>
+
+        <ContactForm ref={form} onSubmit={handleSubmit}>
+          <ContactTitle>Send me an Email</ContactTitle>
+          
+          <ContactInput
+            type="text"
+            name="user_name"
+            placeholder="Your Name"
+            required
+          />
+          
+          <ContactInput
+            type="email"
+            name="user_email"
+            placeholder="Your Email"
+            required
+          />
+          
+          <ContactInputMessage
+            name="message"
+            rows="5"
+            placeholder="Your Message"
+            required
+          />
+          
+          <ContactButton type="submit" value="Send Message" />
+        </ContactForm>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Contact;
---
+++ src/data/constants.js
@@ -0,0 +1,352 @@
+import img from '../images/img.png';
+
+export const Bio = {
+    name: "Rituparna Biswas",
+    roles: [
+      "Full Stack Developer",
+      "Data Science Enthusiast",
+      "Programmer",
+    ],
+    description:
+"I am a motivated and adaptable individual, eager to embrace new challenges and opportunities. Driven by a passion for continuous learning, I am committed to delivering exceptional results. With a positive outlook and a growth-oriented mindset, I am ready to make a meaningful impact and contribute to achieving success.",
+    github: "https://github.com/rituparna25",
+    resume:
+      "https://www.canva.com/design/DAGgUTw8zqw/vRqI0axaefC7VMYVEuK9og/view?utm_content=DAGgUTw8zqw&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h69989748ec",
+    linkedin: "https://www.linkedin.com/in/rituparna-biswas-30362a2b8/",
+    insta: "https://www.instagram.com/rituparnaaah/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "Tailwind CSS",
+          image:"https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg"
+        }
+        
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Express Js",
+          image:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png"
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+      ],
+    },
+    {
+      title: "Programming Languages",
+      skills: [
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "C",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg",
+        },
+        {
+          name: "C++",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg",
+        },
+        {
+          name: "Java",
+          image:
+            "https://upload.wikimedia.org/wikipedia/de/e/e1/Java-Logo.svg",
+        },
+        {
+          name: "R",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/1/1b/R_logo.svg",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+        },
+        {
+          name: "Canva",
+          image:"https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/canva-icon.png"
+        },
+        {
+          name: "Figma",
+          image:
+            "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+        },
+        {
+          name: "Qlik Sense",
+          image:"https://upload.wikimedia.org/wikipedia/commons/3/32/Qlik_Logo.svg"
+        },
+        {
+          name: "Jupyter",
+          image:"https://upload.wikimedia.org/wikipedia/commons/3/38/Jupyter_logo.svg"
+        }
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/commons/a/a3/Indian_Oil_Logo.svg",
+      role: "Data Analytics Intern",
+      company: "IOCL",
+      date: "Dec 2024 - Jan 2025",
+      desc: "Developed a customer churn prediction model using ML algorithms. Conducted EDA and built Qlik Sense dashboards for real-time insights. Identified key customer retention factors to improve decision-making.",
+      skills: ["Python","SQL","Qliksense"],
+      doc: "https://drive.google.com/file/d/1qyna6WfjbVXqq5BrI-6vEz38zLcHF58G/view?usp=sharing",
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/en/b/be/Heritage_Institute_of_Kolkata_Logo.png",
+      school: "Heritage Institute of Technology , Kolkata",
+      date: "Oct 2022 - present",
+      grade: "9.23 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering (Data Science) at Heritage Institute of Technology , Kolkata. I have completed 5 semesters and have a CGPA of 9.23. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Organization and Architecture, among others.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering (Data Science)",
+    },
+    {
+      id: 1,
+      img: img,
+      school: "The Modern Academy, Kolkata",
+      date: "Oct 2020 - Oct 2022",
+      grade: "96.5%",
+      desc: "I completed my class 12 high school education at The Modern Academy, Kolkata, where I studied Computer Science.",
+      degree: "ISC(XII), Science with Computer",
+    },
+    {
+      id: 2,
+      img: img,
+      school: "The Modern Academy, Kolkata",
+      date: "Apr 2012 - Apr 2020",
+      grade: "97.3%",
+      desc: "I completed my class 10 education at The Modern Academy, Kolkata, where I studied Science with Computer Application.",
+      degree: "ICSC(X), Science with Computer",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 2,
+      title: "TextApp",
+      date: "Jul 2024 - Aug 2024",
+      description:"TextApp is a word counter and a character counting utility which can be used to manipulate your text in the way you want.",      
+      image:
+        "https://github.com/rituparna25/text_utils/blob/main/textapp.png?raw=true",
+      tags: [
+        "React Js",
+        "HTML",
+        "CSS",
+      ],
+      category: "react web app",
+      github: "https://github.com/rituparna25/text_utils",
+      webapp: "https://rituparna25.github.io/text_utils/",
+    },
+    {
+      id: 0,
+      title: "NewsMonkey",
+      date: "Aug 2024 - Sept 2024",
+      description:"NewsMonkey is a news app that gives you news from all around the world in different categories like sports , entertainment , general etc.",
+      image:
+        "https://github.com/rituparna25/Newsapp/blob/master/newsapp.png?raw=true",
+      tags: [
+        "React Js",
+        "HTML",
+        "CSS",
+      ],
+      category: "react web app",
+      github: "https://github.com/rituparna25/Newsapp",
+    },
+    {
+      id: 1,
+      title: "iNotebook",
+      date: "Sept 2024 - Nov 2024",
+      description:"iNotebook is a secure web app to store notes, edit notes, delete and update notes securely in MongoDB Atlas cloud",
+           image:
+        "https://github.com/rituparna25/notebook/blob/main/notebook.png?raw=true",
+      tags: ["React Js", "MongoDb", "Node Js", "Express Js"],
+      category: "full stack web app",
+      github: "https://github.com/rituparna25/notebook",
+      webapp: "https://inotebook-frontend-hn7p.onrender.com",
+    },
+    {
+      id: 7,
+      title: "ExpertConnect",
+      date: "Sept 2024 - present",
+      description:"ExpertConnect leverages large language models and machine learning to match experts with interview boards based on candidates' skills and domains. By embedding profiles and calculating similarity scores, it delivers a scalable and precise solution for expert-candidate matching through an intuitive and visually appealing website.",
+      image:
+        "https://github.com/navya-555/ExpertConnect/blob/main/expertconnect.png?raw=true",
+      tags: [
+        "HTML",
+        "CSS",
+        "Tailwind CSS",
+        "Python",
+        "Flask",
+        "Javascript",
+        "Jupyter Notebook",
+      ],
+      category: "flask app",
+      github: "https://github.com/navya-555/ExpertConnect",
+      member: [
+        {
+          name: "Rituparna Biswas",
+          img:"https://avatars.githubusercontent.com/u/145699840?v=4",
+          linkedin: "https://www.linkedin.com/in/rituparna-biswas-30362a2b8/",
+          github: "https://github.com/rituparna25",
+        },
+        {
+          name: "Navya Choudhury",
+          img:"https://avatars.githubusercontent.com/u/151357977?v=4",
+          linkedin: "https://www.linkedin.com/in/socialnavya/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+          github: "https://github.com/navya-555",
+        },
+        {
+          name: "Mohammad Sufyan",
+          img:"https://avatars.githubusercontent.com/u/151357963?v=4",
+          linkedin: "http://www.linkedin.com/in/social-sufyan",
+          github: "https://github.com/sufyan-555",
+        },
+        {
+          name: "Kanhaiya Yadav",
+          img:"https://avatars.githubusercontent.com/u/125747071?v=4",
+          linkedin: "https://www.linkedin.com/in/kanhaiyadav/",
+          github: "https://github.com/kanhaiyadav",
+        },
+        {
+          name: "Pratik Kumar Singh",
+          img:"https://avatars.githubusercontent.com/u/129691884?v=4",
+          linkedin: "https://www.linkedin.com/in/pratik-singh-53b297254/",
+          github: "https://github.com/pratiks05",
+        },
+        {
+          name: "Dhruv Jain",
+          img:"https://avatars.githubusercontent.com/u/146191158?v=4",
+          linkedin: "https://www.linkedin.com/in/dhruv-jain-205343175",
+          github: "https://github.com/dhruvj27",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Rock-Paper-Scissors",
+      date: "Jan 2024 - Feb 2024",
+      description:"A hand-gesture game where players select rock, paper, or scissors to compete against the computer.",
+      image:"https://github.com/rituparna25/rock-paper-scissors/blob/main/rock_paper.png?raw=true",
+      tags: [ "HTML",
+        "CSS","Javascript"],
+      category: "javascript website game",
+      github: "https://github.com/rituparna25/rock-paper-scissors",
+      webapp: "https://rituparna25.github.io/rock-paper-scissors/",
+    },
+    {
+      id: 3,
+      title: "Tic-Tac-Toe",
+      date: "Jan 2024 - Feb 2024",
+      description:"A 2-player grid-based game where the goal is to align three marks in a row, column, or diagonal to win.",
+      image:"https://github.com/rituparna25/tic-tac-toe_js/blob/main/tictactoe.png?raw=true",
+      tags: [ "HTML",
+        "CSS","Javascript"],
+      category: "javascript website game",
+      github: "https://github.com/rituparna25/tic-tac-toe_js",
+      webapp: "https://rituparna25.github.io/tic-tac-toe_js/",
+    },
+    {
+      id: 5,
+      title: "Simon Game",
+      date: "Jan 2024 - Feb 2024",
+      description:"A memory-based website game where players repeat increasingly complex sequences of flashing lights and sounds.",
+      image:
+        "https://github.com/rituparna25/simon_game_challenge/blob/main/simon.png?raw=true",
+      tags: [ "HTML","CSS","Javascript"],
+      category: "javascript website game",
+      github: "https://github.com/rituparna25/simon_game_challenge",
+      webapp: "https://rituparna25.github.io/simon_game_challenge/",
+    },
+    {
+      id: 6,
+      title: "Currency Converter",
+      date: "Feb 2024 - Mar 2024",
+      description:"A web app that allows users to convert between various currencies using real-time exchange rates.",
+      image:"https://github.com/rituparna25/currency-converter/blob/main/curr_convert.png?raw=true",
+      tags: [ "HTML","CSS","Javascript"],
+      category: "web app",
+      github: "https://github.com/rituparna25/currency-converter",
+      webapp: "https://rituparna25.github.io/currency-converter/",
+    },
+    {
+      id: 8,
+      title: "Calculator",
+      date: "Jun 2023",
+      description:"a python tkinter project, that is a functional calculator",
+      image:"https://github.com/rituparna25/tkinter-calculator_project/blob/main/calculator.png?raw=true",
+      tags: ["Python","Tkinter"],
+      category: "Tkinter GUI interface",
+      github: "https://github.com/rituparna25/tkinter-calculator_project",
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2023, text: "Started my journey in web developement" },
+    { year: 2025, text: "Worked as a data analytics intern" },
+  ];
